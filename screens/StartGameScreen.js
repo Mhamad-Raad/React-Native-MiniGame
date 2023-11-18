34304f4b1a55e4b44c64f6712e9586a3efde1364
mhamad raad <hamaraad883@gmail.com>
2023-11-19 00:51:11 +0300

    add button styles and structure

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,14 +12,21 @@ const StartGameScreen = () => {
         autoCapitalize='none'
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
+    alignItems: 'center',
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
@@ -33,6 +40,7 @@ const styles = StyleSheet.create({
   },
   numberInput: {
     height: 50,
+    width: 50,
     fontSize: 32,
     borderColor: '#ddb52f',
     color: '#ddb52f',
@@ -40,6 +48,12 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    fex: 1,
   },
 });
 
