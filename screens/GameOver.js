00f40a8c9ec2921237efd4138844d4dd39b6ad20
mhamad raad <hamaraad883@gmail.com>
2023-11-27 21:05:03 +0300

    add needed assets / add some styles to the game over screen

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
+
+import Title from '../components/ui/Title';
+
+import Colors from '../constants/colors';
 
 const GameOver = () => {
   return (
-    <View>
-      <Text>The Game is Over!</Text>
+    <View style={styles.rootScreen}>
+      <Title>Game Over!</Title>
+      <View style={styles.iamgeContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/success.png')}
+        />
+      </View>
+      <Text>Your Phone needed x rounds to guess the number y.</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iamgeContainer: {
+    width: 400,
+    height: 400,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: Colors.primary700,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default GameOver;
